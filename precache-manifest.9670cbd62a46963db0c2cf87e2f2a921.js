0b957e7a3d58bc8ebffc2b47439b769210784377
Nathan-Nhat <trantrungnhat6196@gmail.com>
2019-12-20 23:02:36 +0700

    Updates

---
+++ precache-manifest.9670cbd62a46963db0c2cf87e2f2a921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c997a7fe9661a0373e0b0eb141413aff",
+    "url": "/randomVIB/index.html"
+  },
+  {
+    "revision": "e1fd0958897290d8eb5f",
+    "url": "/randomVIB/static/css/main.7117a030.chunk.css"
+  },
+  {
+    "revision": "7d065276d324edf53438",
+    "url": "/randomVIB/static/js/2.f4c05791.chunk.js"
+  },
+  {
+    "revision": "e1fd0958897290d8eb5f",
+    "url": "/randomVIB/static/js/main.ef666627.chunk.js"
+  },
+  {
+    "revision": "1a5beb79202850b0abc4",
+    "url": "/randomVIB/static/js/runtime-main.90eee64d.js"
+  },
+  {
+    "revision": "963ade47438db1ce1c6135a30cf27397",
+    "url": "/randomVIB/static/media/anh.963ade47.jpg"
+  }
+]);
